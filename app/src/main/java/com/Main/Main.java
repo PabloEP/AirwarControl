c0e87fae4d5573189a44f26940e87a12b0316e16
Pablo <pabloep96@gmail.com>
2017-04-22 13:38:36 -0600

    FINAL

--- app/src/main/java/com/Main/Main.java
+++ app/src/main/java/com/Main/Main.java
@@ -1,5 +1,9 @@
 package com.Main;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,27 +34,46 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.*;
 
 
-public class Main extends AppCompatActivity {
+public class Main extends AppCompatActivity implements SensorEventListener {
 
-    private static int portnumber;
-    private static String hostname, IPNodo;
+    private static int portnumber = 1101;
+    private static String hostname = "192.168.0.113"; //<---------------------------------------------------------------PONER LA NUEVA IP
+    private String horizontal, vertical, accion;
+    private TextView PuntosText, MemoriaText, NivelText, HorText, VerText, ActText;
+    private Sensor mySensor;
+    private SensorManager SM;
+    private int disparo = 0;
     private Button btnConectar;
     private static final String debugString = "debug";
     Nodo nodo = new Nodo();
     Gson gson = new Gson();
     JSONObject jsonObject = new JSONObject();
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creador del Manejador del Sensor
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
 
+        //Creador del sensor acelerometro
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Registrar Sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Asignar text
+        PuntosText = (TextView)findViewById(R.id.txtPuntos);
+        MemoriaText = (TextView)findViewById(R.id.txtMemoria);
+        NivelText = (TextView)findViewById(R.id.txtNivel);
+        HorText = (TextView)findViewById(R.id.txtHorizontal);
+        VerText = (TextView)findViewById(R.id.txtVertical);
+        ActText = (TextView)findViewById(R.id.txtAccion);
+        System.out.println("========================================================================================================================================================================");
 
         //Accion del boton
-        btnConectar = (Button) findViewById(R.id.btnConectar);
+        btnConectar = (Button) findViewById(R.id.btnShoot);
         btnConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,42 +81,14 @@ public class Main extends AppCompatActivity {
                 new Thread() {
                     @Override
                     public void run() {
-                        //Base 64
-                        /*
-                        Base
-                        64
-                        String testValue = "32515";
-                        byte[] encodeValue = Base64.encode(testValue.getBytes(), Base64.DEFAULT);
-                        byte[] decodeValue = Base64.decode(encodeValue, Base64.DEFAULT);
-                        String encode, decode;
-                        encode = new String(encodeValue);
-                        decode = new String(decodeValue);
-
-                        Log.i(debugString, "defaultValue = " + testValue);
-                        Log.i(debugString, "encodeValue = " + encode);
-                        Log.i(debugString, "decodeValue = " + decode);
-                        */
-
-                        EditText txtIPManager = (EditText) findViewById(R.id.txtIPManager);
-                        EditText txtPuertoManager = (EditText) findViewById(R.id.txtPuertoManager);
-                        EditText txtPuertoNodo = (EditText) findViewById(R.id.txtPuertoNodo);
-                        EditText txtBytes = (EditText) findViewById(R.id.txtBytes);
-                        EditText txtID = (EditText) findViewById(R.id.txtID);
-                        EditText txtTel = (EditText) findViewById(R.id.txtTel);
-                        IPNodo = obtenerIP();
-                        portnumber = Integer.parseInt(txtPuertoManager.getText().toString());
-                        hostname = txtIPManager.getText().toString();
-
-
+                        disparo += 1;
                         Socket socket;
+
                         try {
                             Log.i(debugString, "Llenando el Json");
-                            jsonObject.put("NodoTel", Integer.parseInt(txtTel.getText().toString()));
-                            jsonObject.put("NodoID", txtID.getText().toString());
-                            jsonObject.put("Bytes", Integer.parseInt(txtBytes.getText().toString()));
-                            jsonObject.put("NodoPort", Integer.parseInt(txtPuertoNodo.getText().toString()));
-                            jsonObject.put("NodoIP", IPNodo);
-                            jsonObject.put("TipoAccion", 7);
+                            jsonObject.put("Horizontal", null);
+                            jsonObject.put("Vertical", null);
+                            jsonObject.put("Accion", "Disparando");
 
 
                             Log.i(debugString, jsonObject.toString());
@@ -132,51 +128,215 @@ public class Main extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    private String obtenerIP() {
-        String ip = "";
-        try {
-            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
-                    .getNetworkInterfaces();
-            while (enumNetworkInterfaces.hasMoreElements()) {
-                NetworkInterface networkInterface = enumNetworkInterfaces
-                        .nextElement();
-                Enumeration<InetAddress> enumInetAddress = networkInterface
-                        .getInetAddresses();
-                while (enumInetAddress.hasMoreElements()) {
-                    InetAddress inetAddress = enumInetAddress.nextElement();
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if( event.values[0] > 2){
+            horizontal = "IZQUIERDA";
+            new Thread() {
+                @Override
+                public void run() {
+                    Socket socket;
 
-                    if (inetAddress.isSiteLocalAddress()) {
-                        ip = inetAddress.getHostAddress();
+                    try {
+                        Log.i(debugString, "Llenando el Json");
+                        jsonObject.put("Horizontal", horizontal);
+                        jsonObject.put("Vertical", vertical);
+                        jsonObject.put("Accion", null);
+
+
+                        Log.i(debugString, jsonObject.toString());
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
+                    try {
+                        Log.i(debugString, "Tratando de conectar");
 
+                        socket = new Socket(hostname, portnumber);
+                        Log.i(debugString, "Coneccion establecida");
+
+
+                        //Enviar json
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bw.write(jsonObject.toString());
+                        bw.newLine();
+                        bw.flush();
+
+                        //
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        Log.i(debugString, "Llega del Server:");
+                        String recibido = br.readLine();
+                        Log.i(debugString, recibido);
+
+
+
+                    } catch (IOException e) {
+                        Log.i(debugString, "Se cayó la conexion");
+                    }
                 }
 
-            }
+            }.start();
+        }else if(event.values[0] < -2){
+            horizontal = "DERECHA" ;
+            new Thread() {
+                @Override
+                public void run() {
+                    Socket socket;
 
-        } catch (SocketException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            ip += "Problemas de IP " + e.toString() + "\n";
+                    try {
+                        Log.i(debugString, "Llenando el Json");
+                        jsonObject.put("Horizontal", horizontal);
+                        jsonObject.put("Vertical", vertical);
+                        jsonObject.put("Accion", null);
+
+
+                        Log.i(debugString, jsonObject.toString());
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Log.i(debugString, "Tratando de conectar");
+
+                        socket = new Socket(hostname, portnumber);
+                        Log.i(debugString, "Coneccion establecida");
+
+
+                        //Enviar json
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bw.write(jsonObject.toString());
+                        bw.newLine();
+                        bw.flush();
+
+                        //
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        Log.i(debugString, "Llega del Server:");
+                        String recibido = br.readLine();
+                        Log.i(debugString, recibido);
+
+
+
+                    } catch (IOException e) {
+                        Log.i(debugString, "Se cayó la conexion");
+                    }
+                }
+
+            }.start();
+        }
+        if(event.values[1] > 2){
+            vertical = "ABAJO";
+            new Thread() {
+                @Override
+                public void run() {
+                    Socket socket;
+
+                    try {
+                        Log.i(debugString, "Llenando el Json");
+                        jsonObject.put("Horizontal", horizontal);
+                        jsonObject.put("Vertical", vertical);
+                        jsonObject.put("Accion", null);
+
+
+                        Log.i(debugString, jsonObject.toString());
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Log.i(debugString, "Tratando de conectar");
+
+                        socket = new Socket(hostname, portnumber);
+                        Log.i(debugString, "Coneccion establecida");
+
+
+                        //Enviar json
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bw.write(jsonObject.toString());
+                        bw.newLine();
+                        bw.flush();
+
+                        //
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        Log.i(debugString, "Llega del Server:");
+                        String recibido = br.readLine();
+                        Log.i(debugString, recibido);
+
+
+
+                    } catch (IOException e) {
+                        Log.i(debugString, "Se cayó la conexion");
+                    }
+                }
+
+            }.start();
+        }else if(event.values[1] < -2 ){
+            vertical = "ARRIBA";
+            new Thread() {
+                @Override
+                public void run() {
+                    Socket socket;
+
+                    try {
+                        Log.i(debugString, "Llenando el Json");
+                        jsonObject.put("Horizontal", horizontal);
+                        jsonObject.put("Vertical", vertical);
+                        jsonObject.put("Accion", null);
+
+
+                        Log.i(debugString, jsonObject.toString());
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Log.i(debugString, "Tratando de conectar");
+
+                        socket = new Socket(hostname, portnumber);
+                        Log.i(debugString, "Coneccion establecida");
+
+
+                        //Enviar json
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        bw.write(jsonObject.toString());
+                        bw.newLine();
+                        bw.flush();
+
+                        //
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        Log.i(debugString, "Llega del Server:");
+                        String recibido = br.readLine();
+                        Log.i(debugString, recibido);
+
+
+
+                    } catch (IOException e) {
+                        Log.i(debugString, "Se cayó la conexion");
+                    }
+                }
+
+            }.start();
         }
 
-        return ip;
-    }
-    /*
-    public static String cifrarBase64(String a) {
-        Base64. encoder = Base64.getEncoder();
-        String b = encoder.encodeToString(a.getBytes(StandardCharsets.UTF_8));
-        return b;
-    }
 
-    public static String descifrarBase64(String a) {
-        Base64 decoder = Base64.getDecoder();
-        byte[] decodedByteArray = decoder.decode(a);
-
-        String b = new String(decodedByteArray);
-        return b;
+        //FUERA DEL THREAD
+        ActText.setText("Accion: Disparo " + disparo);
+        HorText.setText("Horizontal: " + horizontal);
+        VerText.setText("Vertical: " + vertical);
+        if( -2 < event.values[0] & event.values[0] < 2 ){
+            horizontal = null;
+        }
+        if(-2 < event.values[1] & event.values[1] < 2 ){
+            vertical = null;
+        }
     }
-    */
 
 
 }
